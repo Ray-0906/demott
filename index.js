ff8428f99355910a9bd37305071310cd974498c7
Ray-0906 <astra3716@gmail.com>
2025-10-01 08:43:32 +0530

    monitoring with grafana and prometheus

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import client from 'prom-client';
 const app = express();
 const port = 3000;
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({timeout: 5000});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, World!',
     service : "Hello node ",
